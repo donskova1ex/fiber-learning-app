fb0f0f7c1910185d6cab3a6868eb077fdd28f10c
Alex Donskov <donskov_a@icloud.com>
2025-10-08 08:13:22 +0500

    add h1

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,10 @@ import (
 	"go-fiber/config"
 	"go-fiber/internal/home"
 	"go-fiber/pkg/logger"
-	"strings"
 
 	"github.com/gofiber/contrib/fiberzerolog"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
@@ -21,21 +19,8 @@ func main() {
 	logCfg := config.NewLogConfig()
 
 	customLogger := logger.NewLogger(logCfg)
-	engine := html.New("./html", ".html")
-	engine.AddFuncMap(map[string]interface{}{
-		"ToUpper": func(s string) string {
-			switch s {
-			case "":
-				return ""
-			default:
-				return strings.ToUpper(s)
-			}
-		},
-	})
 
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
+	app := fiber.New()
 	app.Use(fiberzerolog.New(fiberzerolog.Config{
 		Logger: customLogger,
 	}))
--- internal/home/handler.go
+++ internal/home/handler.go
@@ -1,6 +1,9 @@
 package home
 
 import (
+	"go-fiber/pkg/t_adapter"
+	"go-fiber/views"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/rs/zerolog"
 )
@@ -20,29 +23,13 @@ func NewHandler(router fiber.Router, customLogger *zerolog.Logger) {
 		router:       router,
 		customLogger: customLogger,
 	}
-	api := handler.router.Group("/api")
-	api.Get("/", handler.home)
-	api.Get("/error", handler.error)
+	handler.router.Get("/", handler.home)
+	handler.router.Get("/error", handler.error)
 }
 
 func (h *HomeHandler) home(c *fiber.Ctx) error {
-
-	users := []Users{
-		{Id: 1, Name: "Alex"},
-		{Id: 2, Name: "Don"},
-		{Id: 3, Name: "Bob"},
-	}
-	names := []string{"Anna", "John", "Mike", "Bob", "Don", "Alex", "Kate", "Max", "Sara"}
-
-	data := struct {
-		Users []Users
-		Names []string
-	}{
-		Users: users,
-		Names: names,
-	}
-
-	return c.Render("page", data)
+	component := views.Main()
+	return t_adapter.Render(c, component)
 }
 
 func (h *HomeHandler) error(c *fiber.Ctx) error {
