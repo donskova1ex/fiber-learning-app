ab95a8c7eb5c8d773a4c6491ac6db1f98ea69972
Alex Donskov <donskov_a@icloud.com>
2025-10-15 09:39:36 +0500

    add menu

--- views/layout/menu_templ.go
+++ views/layout/menu_templ.go
@@ -8,6 +8,8 @@ package layout
 import "github.com/a-h/templ"
 import templruntime "github.com/a-h/templ/runtime"
 
+import "go-fiber/views/components"
+
 func Menu() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
@@ -33,7 +35,33 @@ func Menu() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"menu\"><div class=\"menu_logo\">Logo</div><div class=\"menu_right\"><a class=\"menu_right_link\">Войти</a></div></div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"menu\"><img src=\"/public/images/logo.svg\"><div class=\"menu_right\"><a class=\"menu_right_link\">Войти</a>")
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Var2 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
+			templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
+			templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
+			if !templ_7745c5c3_IsBuffer {
+				defer func() {
+					templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
+					if templ_7745c5c3_Err == nil {
+						templ_7745c5c3_Err = templ_7745c5c3_BufErr
+					}
+				}()
+			}
+			ctx = templ.InitializeContext(ctx)
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<span>Зарегистрироваться</span>")
+			if templ_7745c5c3_Err != nil {
+				return templ_7745c5c3_Err
+			}
+			return nil
+		})
+		templ_7745c5c3_Err = components.LinkButton("/register").Render(templ.WithChildren(ctx, templ_7745c5c3_Var2), templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "</div></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -57,12 +85,12 @@ func MenuStyle() templ.Component {
 			}()
 		}
 		ctx = templ.InitializeContext(ctx)
-		templ_7745c5c3_Var2 := templ.GetChildren(ctx)
-		if templ_7745c5c3_Var2 == nil {
-			templ_7745c5c3_Var2 = templ.NopComponent
+		templ_7745c5c3_Var3 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var3 == nil {
+			templ_7745c5c3_Var3 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, "<style>\n    \t    .menu{\n    \t        display: flex;\n    \t        justify-content: space-between;\n    \t        padding: 20px 0;\n    \t    }\n    \t    .menu_right{\n    \t        display: flex;\n    \t        gap: 20px;\n    \t    }\n    \t    .menu_right_link{\n    \t        text-decoration: none;\n    \t        color: var(--color-white);\n    \t        font-size: 16px;\n    \t        cursor: pointer;\n    \t    }\n    \t    .menu_right_link:hover {\n    \t        color: #9f9f9f;\n    \t    }\n    \t</style>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "<style>\n    \t    .menu{\n    \t        display: flex;\n    \t        justify-content: space-between;\n    \t        padding: 20px 0;\n    \t        max-width: 1290px;\n    \t        align-items: center;\n    \t        width: 100%;\n    \t    }\n    \t    .menu_right{\n    \t        display: flex;\n    \t        gap: 20px;\n    \t        align-items: center;\n    \t    }\n    \t    .menu_right_link{\n    \t        text-decoration: none;\n    \t        color: var(--color-white);\n    \t        font-size: 16px;\n    \t        cursor: pointer;\n    \t    }\n    \t    .menu_right_link:hover {\n    \t        color: #9f9f9f;\n    \t    }\n    \t</style>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
